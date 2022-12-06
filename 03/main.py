fd9a3f20f4f64d271f626d5e7809a10809088add
Elliot Voris <elliot@voris.me>
2022-12-05 19:31:30 -0600

    day 03 - part 02
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- 03/main.py
+++ 03/main.py
@@ -5,17 +5,23 @@ with open('input.txt', 'r') as f:
     sacks = f.read().splitlines()
 
 alphabet = list(string.ascii_letters)
-
 priority_sum = 0
 
-for sack in sacks:
-    # print(f"{sack[:len(sack)//2]} - {sack[len(sack)//2:]}")
-    comp1 = sack[:len(sack)//2]
-    comp2 = sack[len(sack)//2:]
-    for item in comp1:
-        if item in comp2:
-            # print(f"{item}: {alphabet.index(item) + 1}pts")
-            priority_sum += alphabet.index(item) + 1
+# for sack in sacks:
+#     comp1 = sack[:len(sack)//2]
+#     comp2 = sack[len(sack)//2:]
+#     for item in comp1:
+#         if item in comp2:
+#             priority_sum += alphabet.index(item) + 1
+#             break
+
+for group_sacks in [[sacks[i:i+3]] for i in range(0, len(sacks), 3)]:
+    sack0 = group_sacks[0][0]
+    sack1 = group_sacks[0][1]
+    sack2 = group_sacks[0][2]
+    for char in sack0:
+        if char in sack1 and char in sack2:
+            priority_sum += alphabet.index(char) + 1
             break
 
 print(priority_sum)
