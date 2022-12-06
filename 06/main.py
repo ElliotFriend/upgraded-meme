c6630173800ee1140c462b6e47b5ea6710e5ec74
Elliot Voris <elliot@voris.me>
2022-12-05 23:11:21 -0600

    day 06 - part 02
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- 06/main.py
+++ 06/main.py
@@ -6,8 +6,9 @@ with open('input.txt', 'r') as f:
 signal = signal[0]
 
 sop_marker_len = 4
+som_marker_len = 14
 for i in range(0, len(signal)):
-    marker = signal[i:sop_marker_len+i]
-    if len(set(marker)) > 3:
-        print(i+sop_marker_len)
+    marker = signal[i:som_marker_len+i]
+    if len(set(marker)) > 13:
+        print(i+som_marker_len)
         break
