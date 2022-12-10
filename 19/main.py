ba0cc7a3f22d9f70115b5fcf887854c8a2aaeff5
Elliot Voris <elliot@voris.me>
2022-12-10 01:30:17 -0600

    putting some more useful placeholder contents in `main.py` files
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- 19/main.py
+++ 19/main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+contents = None
+with open('input.txt', 'r') as f:
+    contents = f.read().splitlines()
+print(contents)
+
+# probly def functions here
+
+for line in contents:
+    # do something here
+    pass
+
+print(f"Part 01: {None}")
+print(f"Part 02: {None}")
