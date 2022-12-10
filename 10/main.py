112b4d8c88b1dd614fa25d6df0cb79cb792b06db
Elliot Voris <elliot@voris.me>
2022-12-10 01:25:20 -0600

    day 10 - cleaning up `main.py` quite a bit.
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- 10/main.py
+++ 10/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import pprint, math
-
 contents = None
 with open('input.txt', 'r') as f:
     contents = f.read().splitlines()
@@ -10,10 +8,9 @@ with open('input.txt', 'r') as f:
 x_reg = 1
 cycle_count = 0
 ss_sum = 0
-# sprite_position = 0
-crt_rows = [[], [], [], [], [], [], []]
+crt_rows = [[], [], [], [], [], []]
 
-def check_cycle_count(value = 0):
+def clock_cycle(value = 0):
     global ss_sum
     global crt_rows
     global x_reg
@@ -21,36 +18,27 @@ def check_cycle_count(value = 0):
 
     row = cycle_count // 40
     col = cycle_count % 40
-    # if (row == 0):
-    #     print(f"row: {row}\t\tcol: {col}\t\tx_reg: {x_reg}\tmath: {cycle_count % 40}")
-    cycle_count += 1
 
     if col in range(x_reg - 1, x_reg + 2):
-        # print(f"cycle: {cycle_count}\tmath: {math.ceil(cycle_count // 40)}")
         crt_rows[row].append('#')
     else:
         crt_rows[row].append('.')
 
+    cycle_count += 1
     x_reg += value
+    # because i'm 0-indexing (not 1) `cycle_count`, i have to add 1 here
     if cycle_count + 1 in [20, 60, 100, 140, 180, 220]:
         ss_sum += (cycle_count + 1) * x_reg
 
 for line in contents:
     if line == 'noop':
-        # cycle_count += 1
-        check_cycle_count()
+        clock_cycle()
     else:
-        value = int(line.split()[1])
-        # cycle_count += 1
-        check_cycle_count()
-        # cycle_count += 1
-        check_cycle_count(value)
-        # x_reg += value
+        clock_cycle()
+        clock_cycle(int(line.split()[1]))
 
+print(f"Part 01: {ss_sum}\n")
 
-# print(x_reg)
-# print(cycle_count)
-print(ss_sum)
-# crt_rows.pop()
+print("Part 02:")
 for row in crt_rows:
     print(''.join(row))
