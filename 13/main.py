ae51109b869aa151f5c614e758f3e42579feb001
Elliot Voris <elliot@voris.me>
2022-12-13 01:46:52 -0600

    day 13 - part 02
    Felt much better about this part! Booyah!
    
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- 13/main.py
+++ 13/main.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
-import ast
+import ast, pprint
 from itertools import zip_longest
 
 contents = None
 with open('input.txt', 'r') as f:
     contents = f.read()
-# print(contents)
-
 packet_pairs = [[ast.literal_eval(l), ast.literal_eval(r)] for [l, r] in [p.split('\n') for p in contents.split('\n\n')]]
 # print(packet_pairs)
+
+all_packets = None
+with open('input.txt', 'r') as f:
+    all_packets = f.read().splitlines()
+while '' in all_packets:
+    all_packets.remove('')
+all_packets = [ast.literal_eval(p) for p in all_packets]
+# print(all_packets)
 # probly def helper functions here
 
 def compare_items(left, right):
@@ -17,7 +23,7 @@ def compare_items(left, right):
     if isinstance(left, list) and isinstance(right, list):
         # both are lists, compare each item
         for l, r in zip_longest(left, right):
-            print(l, r)
+            # print(l, r)
             if l == None:
                 return True
             elif r == None:
@@ -50,14 +56,32 @@ def compare_items(left, right):
 def part_one(input):
     correctly_ordered = []
     for i in range(1, len(input) + 1):
-        if compare_items(input[i-1][0], input[i-1][1]):
+        if compare_items(input[i - 1][0], input[i - 1][1]):
             correctly_ordered.append(i)
     return sum(correctly_ordered)
 
 def part_two(input):
-    # return input
-    pass
+    ordered_packets = [[[2]], [[6]]]
+    # input.append([[2]]).append([[6]])
+    for packet in input:
+        # if i == 0:
+        #     ordered_packets.append(input[i])
+        # else:
+        for i in range(len(ordered_packets)):
+            op = ordered_packets[i]
+            v = compare_items(packet, op)
+            if v == True:
+                ordered_packets.insert(i, packet)
+                break
+            else:
+                pass
+        if packet not in ordered_packets:
+            ordered_packets.append(packet)
+    # pprint.pprint(ordered_packets)
+    index2 = ordered_packets.index([[2]]) + 1
+    index6 = ordered_packets.index([[6]]) + 1
+    return index2 * index6
 
 if __name__ == '__main__':
     print(f"Part 01: {part_one(packet_pairs)}")
-    print(f"Part 02: {part_two(packet_pairs)}")
+    print(f"Part 02: {part_two(all_packets)}")
